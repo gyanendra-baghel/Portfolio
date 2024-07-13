1434c26f0538fe2f0e8ea439e55027bdb1fe6708
Gyanendra Baghel <gyanendrabaghel633@gmail.com>
2024-07-14 00:53:42 +0530

    FIX:Remove unwanted type attribute

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const NotFound: React.FC = () => {
   return (
     <>
-    <Header type='other'/>
-      <div className='wrapper column'>
+      <Header />
+      <div className="wrapper column">
         <h1>404 - Not Found</h1>
-        <p className='headline'>The page you are looking for does not exist.</p>
+        <p className="headline">The page you are looking for does not exist.</p>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 };
